49bbb465fb8ee7efb872dd1a103ef19c639903a7
khalid-talakshi <khalid.talakshi@outlook.com>
2017-10-01 20:25:12 -0400

    Finished up to step 12

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,20 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if (answer || attempt == '') {
+      setHiddenFields();
+    }
 }
 
-//implement new functions here
+function setHiddenFields() {
+  answer =  Math.floor(Math.random())*1000;
+  answer.toString();
+  while (answer.length < 4) {
+    answer = "0" + answer;
+  }
+  attempt = 0;
+}
+
+function setMessage(text) {
+  document.getElementById('message').innerHTML(text);
+}
